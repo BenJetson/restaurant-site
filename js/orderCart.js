667363999d6cd5c3d38a4bb6847b46741e231751
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-03-04 10:32:27 -0500

    Fixed the footer and menu.

--- js/orderCart.js
+++ js/orderCart.js
@@ -5,3 +5,14 @@ $(document).ready(function() {
     });
     
 });
+
+/* 
+
+Originally, I was going to have the script append the items
+you order into the cart area below the menu using jQuery. 
+You can see where the areas originally were in the HTML,
+they're just commented out. 
+
+Unfortunately, I just ran out of time. :)
+
+*/
